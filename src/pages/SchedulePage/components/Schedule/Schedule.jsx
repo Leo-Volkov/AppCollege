2e8d7f205892eccc1949e6f368c7de93d1fc5648
Лев <Leo.Volkov156@yandex.ru>
2024-11-08 21:31:01 +0300

    .

--- src/pages/SchedulePage/components/Schedule/Schedule.jsx
+++ src/pages/SchedulePage/components/Schedule/Schedule.jsx
@@ -4,6 +4,7 @@ import ColGryp from './components/ColGryp/ColGryp';
 import CartNumLess from './components/CartNumLess/CartNumLess.jsx';
 import useSortScheduleDey from '../../hook/useSortSchedule.js';
 
+import { motion } from 'framer-motion';
 
 import './Schedule.css';
 
@@ -13,29 +14,26 @@ export default function ScheduleApp() {
   const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const scheduleDey = useMemo(() => [...scheduleJSON.scheduleDey], []);
   const [numGrup, setNumGrup] = useState([0, 1, 2]);
-  
-  useEffect(() => {
-    const interval = setInterval(() => {
+
+  useMemo(() => {
+    setInterval(() => {
       setNumGrup((prevNumGrup) => {
         let newArr = prevNumGrup.map((num) => {
           const nextNum = num + 3;
           return nextNum < scheduleDey.length ? nextNum : null;
         });
-  
+
         // Если только первый элемент стал null, начинаем заново
         if (newArr[0] === null) {
           newArr = [0, 1, 2];
         }
-        
+
         // Фильтруем значения, чтобы выводить только те, которые не равны null
-        let filteredNumGrup = newArr.filter((num) => num !== null)
+        let filteredNumGrup = newArr.filter((num) => num !== null);
         return filteredNumGrup;
       });
     }, 7000);
-  
-    return () => clearInterval(interval);
-  }, [scheduleDey.length]);
-
+  }, []);
 
   return (
     <div className="Schedule_content">
@@ -49,11 +47,17 @@ export default function ScheduleApp() {
         })}
       </div>
       <div className="col_Gryp_content">
-        {useSortScheduleDey(scheduleDey, numGrup).map((el, index) => {
+        {useSortScheduleDey(scheduleDey, numGrup).map((el) => {
           return (
-            <section key={index}>
+            <motion.section
+              key={el.grup}
+              initial={{ opacity: 0.5, scale: 0.95 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0.4, scale: 0.8 }}
+              transition={{ duration: 2, ease: 'easeInOut' }}
+            >
               <ColGryp scheduleGryp={el} />
-            </section>
+            </motion.section>
           );
         })}
       </div>
