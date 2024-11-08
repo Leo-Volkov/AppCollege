762cc4f0889ce81dce8e58b8e23918c87ce16597
Лев <Leo.Volkov156@yandex.ru>
2024-11-08 13:19:32 +0300

    Update useSortSchedule.js

--- src/pages/SchedulePage/hook/useSortSchedule.js
+++ src/pages/SchedulePage/hook/useSortSchedule.js
@@ -1,13 +1,9 @@
-export default function useSortScheduleDey(scheduleDey, numGrup ) {
+export default function useSortScheduleDey(scheduleDey, numGrup) {
   const result = [];
-  console.log(numGrup);
-  console.log(scheduleDey);
-  
+
   for (let i = 0; i < numGrup.length; i++) {
     result.push(scheduleDey[numGrup[i]]);
   }
-  
-  console.log(result);
-  
+
   return result;
 }
