f6a524ee9eea37985eec6d17637a092c95ecb9ac
Лев <Leo.Volkov156@yandex.ru>
2024-11-13 20:49:30 +0300

    .

--- src/pages/SchedulePage/components/Schedule/components/ColGryp/ColGryp.jsx
+++ src/pages/SchedulePage/components/Schedule/components/ColGryp/ColGryp.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import './ColGryp.css';
 import PropTypes from 'prop-types';
 
@@ -6,15 +7,46 @@ ColGryp.propTypes = {
 };
 
 export default function ColGryp({ scheduleGryp }) {
+  const [currentLesson, setCurrentLesson] = useState(2);
+  const [numBreak, setNumBreak] = useState(null);
+
+  useEffect(() => {
+    const checkTime = () => {
+      const now = new Date();
+  //     const currentTime = now.getHours() * 60 + now.getMinutes();
+
+  //     let activeLessonIndex = null;
+  //     scheduleGryp.subjects.forEach((lesson, index) => {
+  //       const startTime = lesson.startHour * 60 + lesson.startMinute;
+  //       const endTime = lesson.endHour * 60 + lesson.endMinute;
+        
+  //       if (currentTime >= startTime && currentTime <= endTime) {
+  //         activeLessonIndex = index;
+  //       }
+  //     });
+
+  //     setCurrentLesson(activeLessonIndex);
+  //     setIsBreak(activeLessonIndex === null);
+    };
+
+    checkTime();
+    const interval = setInterval(checkTime, 1000); // Update every minute
+
+    return () => clearInterval(interval);
+  }, [scheduleGryp.subjects]);
+
   return (
     <>
       <div className="NameGryp flex_center">
         <h2>{scheduleGryp.group}</h2>
       </div>
       <div className="LessGryp_content">
-        {scheduleGryp.subjects.map((el, i) => {
+        {scheduleGryp.subjects.map((el, index) => {
           return (
-            <div className="less_cart" key={i}>
+            <div
+              key={index}
+              className={`less_cart lesson ${currentLesson ? index === currentLesson - 1 ? 'active' : '' : ''} ${numBreak === index ? 'break_2' : ''} ${numBreak ? (numBreak - 1) === index ? 'break_1' : '' : ''}`}
+              >
               <h3 className="less_Name">{el.subject}</h3>
               <div className="dop_inf">
                 <div className="lecturer">
