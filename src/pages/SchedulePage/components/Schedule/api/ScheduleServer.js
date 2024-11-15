378ed44364b85fb6f7be390024c56a28b842da37
Лев <Leo.Volkov156@yandex.ru>
2024-11-15 23:43:54 +0300

    Update ScheduleServer.js

--- src/pages/SchedulePage/components/Schedule/api/ScheduleServer.js
+++ src/pages/SchedulePage/components/Schedule/api/ScheduleServer.js
@@ -7,42 +7,20 @@ export default class ScheduleServer {
         sse.onopen = () => console.log('>>> Connection opened!');
 
         sse.onmessage = (event) => {
-          try {
-            const parsedData = JSON.parse(event.data);
-            if (!parsedData || !parsedData.ScheduleWeek) {
-              throw new Error('Invalid data format');
-            }
-            const res = parsedData.ScheduleWeek;
-            console.log(res);
-            localStorage.setItem('localScheduleJSON', JSON.stringify(res));
-            resolve(res); // Возвращаем данные
-            sse.close(); // Закрываем соединение
-          } catch (err) {
-            reject(err);
-          }
+          const parsedData = JSON.parse(event.data);
+          const res = parsedData.ScheduleWeek;
+          console.log(res);
+          localStorage.setItem('localScheduleJSON', JSON.stringify(res));
+          resolve(res); // Возвращаем данные
+          sse.close(); // Закрываем соединение
         };
 
         sse.onerror = (err) => {
-          console.error('Error: ', err);
-          const cachedData = localStorage.getItem('localScheduleJSON');
-          if (cachedData) {
-            try {
-              const parsedData = JSON.parse(cachedData);
-              if (parsedData && Array.isArray(parsedData)) {
-                resolve(parsedData); // Возвращаем кэшированные данные
-              } else {
-                throw new Error('Cached data is invalid');
-              }
-            } catch (parseErr) {
-              reject(new Error('Failed to parse cached data: ' + parseErr));
-            }
-          } else {
-            reject(new Error('No cached data available and connection failed'));
-          }
+          console.error('Error Server: ', err);
           sse.close(); // Закрываем соединение
         };
       } catch (error) {
-        reject(error); // Ловим глобальные ошибки
+        reject("Глобальная ошибка: " + error); // Ловим глобальные ошибки
       }
     });
   }
