9d7e7610ccb049f24383c1859e7afbb04003fc1a
Лев <Leo.Volkov156@yandex.ru>
2024-11-15 23:03:23 +0300

    Update ScheduleServer.js

--- src/pages/SchedulePage/components/Schedule/api/ScheduleServer.js
+++ src/pages/SchedulePage/components/Schedule/api/ScheduleServer.js
@@ -1,25 +1,25 @@
 export default class ScheduleServer {
   static getWeek() {
     try {
-      let response = [];
+      const response = [];
       const sse = new EventSource('http://localhost:8080/schedule');
 
       sse.onopen = () => console.log('>>> Connection opened!');
 
-      // Обработка обычных сообщений
+      // // Обработка обычных сообщений
       sse.onmessage = (event) => {
         const res = event.data.ScheduleWeek;
         console.log(res);
-
-        localStorage.setItem('localScheduleJSON', `${res}`);
-        response = res;
       };
+      //   localStorage.setItem('localScheduleJSON', `${res}`);
+      //   response = res;
+      // };
 
-      // Обработка ошибок
-      sse.onerror = (err) => {
-        console.log('Error: ' + err);
-        response = JSON.parse(localStorage.getItem("localScheduleJSON"))
-      };
+      // // Обработка ошибок
+      // sse.onerror = (err) => {
+      //   console.log('Error: ' + err);
+      //   response = JSON.parse(localStorage.getItem("localScheduleJSON"))
+      // };
       return response;
     } catch (error) {
       console.log('Error:  ' + error);
