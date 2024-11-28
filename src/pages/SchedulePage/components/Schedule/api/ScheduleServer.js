a9492e122de56dd40006877c5f56e01921d41d06
Лев <Leo.Volkov156@yandex.ru>
2024-11-28 13:47:35 +0300

    .

--- src/pages/SchedulePage/components/Schedule/api/ScheduleServer.js
+++ src/pages/SchedulePage/components/Schedule/api/ScheduleServer.js
@@ -4,8 +4,8 @@ export default class ScheduleServer {
   static async getWeek(setScheduleWeek, setError) {
     return new Promise(() => {
       try {
-        const sse = new EventSource('http://localhost:8080/schedule');
-        // const sse = new EventSource('http://172.24.1.201:8080/schedule');
+        // const sse = new EventSource('http://localhost:8080/schedule');
+        const sse = new EventSource('http://172.24.1.202:8080/schedule');
 
 
         sse.onopen = () => console.log('>>> Connection opened!');
